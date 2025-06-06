1c6ee2f7331fa975cff420fdffa4f7c3ac1dbfd8
Ali Imran <muhammadimran.math@gmail.com>
2025-06-06 09:06:29 +0500

    wiring up top level fragments with bottom nav

--- app/src/main/java/com/miassolutions/rollcall/MainActivity.kt
+++ app/src/main/java/com/miassolutions/rollcall/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.navigation.NavController
 import androidx.navigation.NavHost
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -21,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,8 +41,41 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment: NavHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
-        binding.toolbar.setupWithNavController(navController)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.statsFragment, R.id.attendanceFragment, R.id.studentsFragment, R.id.settingsFragment)
+        )
+
+
+
+
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
         binding.bottomNavView.setupWithNavController(navController)
+
+        binding.bottomNavView.setOnItemSelectedListener {item ->
+            when(item.itemId){
+                R.id.statsFragment -> {
+                    navController.navigate(R.id.statsFragment)
+                    true
+                }
+
+                R.id.attendanceFragment -> {
+                    navController.navigate(R.id.attendanceFragment)
+                    true
+                }
+
+                R.id.studentsFragment -> {
+                    navController.navigate(R.id.studentsFragment)
+                    true
+                }
+
+
+                R.id.settingsFragment -> {
+                    navController.navigate(R.id.settingsFragment)
+                    true
+                }
+                else ->false
+            }
+        }
 
 
 
