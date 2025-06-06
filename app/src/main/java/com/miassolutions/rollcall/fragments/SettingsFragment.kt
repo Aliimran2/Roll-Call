40e20e4ce1b704815640943f4744469e2f1fcade
Ali Imran <muhammadimran.math@gmail.com>
2025-06-06 08:50:03 +0500

    fragment settings added and menu changes

---
+++ app/src/main/java/com/miassolutions/rollcall/fragments/SettingsFragment.kt
@@ -0,0 +1,30 @@
+package com.miassolutions.rollcall.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.miassolutions.rollcall.R
+import com.miassolutions.rollcall.databinding.FragmentSettingsBinding
+
+
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
+    private var _binding : FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentSettingsBinding.bind(view)
+
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+
+}
