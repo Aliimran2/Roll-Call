b3425e53215acbdd4b67514fba4837316ebcd239
Ali Imran <muhammadimran.math@gmail.com>
2025-06-08 14:31:52 +0500

    Student add fragment basic setup working

--- app/src/main/java/com/miassolutions/rollcall/ui/fragments/AddStudentFragment.kt
+++ app/src/main/java/com/miassolutions/rollcall/ui/fragments/AddStudentFragment.kt
@@ -2,9 +2,13 @@ package com.miassolutions.rollcall.ui.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.miassolutions.rollcall.R
 import com.miassolutions.rollcall.databinding.FragmentAddStudentBinding
+import com.miassolutions.rollcall.utils.StudentProvider
+import com.miassolutions.rollcall.utils.showToast
 
 class AddStudentFragment : Fragment(R.layout.fragment_add_student) {
 
@@ -15,6 +19,35 @@ class AddStudentFragment : Fragment(R.layout.fragment_add_student) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentAddStudentBinding.bind(view)
+
+
+        binding.saveBtn.setOnClickListener{
+
+        setupSaveBtn()
+        }
+
+    }
+    private fun setupSaveBtn(){
+        val rollNumber = binding.etRollNumber.text.toString()
+        val studentName = binding.etName.text.toString()
+
+       when{
+           rollNumber.isBlank() -> {
+               binding.etRollNumber.requestFocus()
+               binding.etRollNumber.error = "Enter roll number"
+           }
+           studentName.isBlank() -> {
+               binding.etName.requestFocus()
+               binding.etName.error = "Enter name of the student"
+           }
+
+           else -> {
+               val roll = rollNumber.toInt()
+               StudentProvider.addStudent(roll, studentName)
+               findNavController().navigateUp()
+               showToast("$studentName is added in db")
+           }
+       }
 
     }
 
